0d4054ed09d01addc44c1e204f8c52bc785a0f23
natanaoliveira06 <143542484+natanaoliveira06@users.noreply.github.com>
2023-10-19 13:04:56 +0100

    FichaTecnica01

--- FichasJava/src/FichaPratica01/Ex_05.java
+++ FichasJava/src/FichaPratica01/Ex_05.java
@@ -1,4 +1,17 @@
 package FichaPratica01;
 
+import java.util.Scanner;
+
 public class Ex_05 {
+
+    public static void main(String[] args) {
+
+        // Instanciar o Scanner
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+
+
+
+    }
 }
