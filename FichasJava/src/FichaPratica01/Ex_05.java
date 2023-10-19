13390ad498d4b6a945b0a5fe95f220c8eba0c23f
natanaoliveira06 <143542484+natanaoliveira06@users.noreply.github.com>
2023-10-19 18:22:38 +0100

    FichaTecnica01

--- FichasJava/src/FichaPratica01/Ex_05.java
+++ FichasJava/src/FichaPratica01/Ex_05.java
@@ -10,8 +10,30 @@ public class Ex_05 {
         Scanner input = new Scanner(System.in);
 
         // Declarar variáveis
+        double valor1, valor2, valor3, medArit, medPond, p1, p2, p3;
 
+        // Ler valor1
+        System.out.print("Insira um número: ");
+        valor1= input.nextInt();
 
+        // Ler valor2
+        System.out.print("Insira um número: ");
+        valor2 = input.nextInt();
 
+        // Ler valor3
+        System.out.print("Insira um número: ");
+        valor3 = input.nextInt();
+
+        // Calcular a média aritmética
+        medArit = (valor1+valor2+valor3)/3;
+
+        // Apresentar a média aritmética
+        System.out.println("Média Aritmética: " + medArit);
+
+        // Calcular a média ponderada
+        medPond = ((valor1*p1) + (valor2*p2) + (valor3*p3)) / p1 + p2+ p3;
+
+        // Apresentar a média ponderada
+        System.out.println("Média Ponderada: " + medPond);
     }
 }
