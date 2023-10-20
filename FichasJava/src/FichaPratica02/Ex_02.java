25cf533de30727c6dd0853c324f2f7a801ceab13
natanaoliveira06 <143542484+natanaoliveira06@users.noreply.github.com>
2023-10-20 16:47:32 +0100

    aula

--- FichasJava/src/FichaPratica02/Ex_02.java
+++ FichasJava/src/FichaPratica02/Ex_02.java
@@ -1,8 +1,27 @@
 package FichaPratica02;
 
-public class Ex_02 {
+import java.util.Scanner;
 
+public class Ex_02 {
     public static void main(String[] args) {
+
+        // Instanciar o Scanner (obrigatório em todos os exercícios)
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        double salario;
+
+        // Ler salário
+        System.out.print("Salário: ");
+        salario = input.nextDouble();
+
+        if (salario <= 15000) {
+            System.out.println("Taxa 20%: " + (salario * 0.2));
+        }
+
+        if (salario > 15000) {
+            System.out.println("Taxa30%: " + (salario * 0.3));
+        }
 
     }
 }
---
+++ FichasJava/src/FichaPratica02/Ex_03.java
@@ -0,0 +1,36 @@
+package FichaPratica02;
+
+import java.util.Scanner;
+
+public class Ex_03 {
+    public static void main(String[] args) {
+
+        // Instanciar o Scanner (obrigatório em todos os exercícios)
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        double salario;
+
+        // Ler salário
+        System.out.print("Salário anual: ");
+        salario = input.nextDouble();
+
+
+        if (salario <= 15000) {
+            System.out.println("Montante de imposto: " + (salario * 0.2));
+        }
+
+        if (salario > 15000 && salario <= 20000) {
+            System.out.println("Montante de imposto: " + (salario * 0.3));
+        }
+
+        if (salario > 20000 && salario <= 25000) {
+            System.out.println("Montante de imposto: " + (salario * 0.35));
+        }
+
+        if (salario > 25000) {
+            System.out.println("Montante de imposto: " + (salario * 0.40));
+        }
+
+    }
+}
