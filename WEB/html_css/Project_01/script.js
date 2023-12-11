c1fa994050fbb9aecc3a7cdbba80a85a36c4f287
natanaoliveira06 <143542484+natanaoliveira06@users.noreply.github.com>
2023-12-11 08:12:12 +0000

    11/12

--- WEB/html_css/Project_01/script.js
+++ WEB/html_css/Project_01/script.js
@@ -1,13 +1,31 @@
-const navLinkEls = document.querySelectorAll(".nav-link");
-const windowPathname = window.location.pathname;
-
-navLinkEls.forEach((navLinkEl) => {
-  const navLinkPathname = new URL(navLinkEl.href).pathname;
+function message() {
+  var firstName = document.getElementById("validationCustom01");
+  var lastName = document.getElementById("validationCustom02");
+  var email = document.getElementById("exampleFormControlInputEmail");
+  var msg = document.getElementById("exampleFormControlTextarea1");
+  const success = document.getElementById("success");
+  const danger = document.getElementById("danger");
 
   if (
-    windowPathname === navLinkPathname ||
-    (windowPathname === "index.html" && navLinkPathname === "/")
+    firstName.value === "" ||
+    lastName.value === "" ||
+    email.value === "" ||
+    msg.value === ""
   ) {
-    navLinkEl.classList.add("active");
+    danger.style.display = "block";
+  } else {
+    setTimeout(() => {
+      firstName.value = "";
+      lastName.value = "";
+      email.value = "";
+      msg.value = "";
+    }, 2000);
+
+    success.style.display = "block";
   }
-});
+
+  setTimeout(() => {
+    danger.style.display = "none";
+    success.style.display = "none";
+  }, 4000);
+}
