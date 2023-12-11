d0bc161a0a7bf088c5254bc256223340d540f174
natanaoliveira06 <143542484+natanaoliveira06@users.noreply.github.com>
2023-12-11 23:09:14 +0000

    11

--- PROGRAMAÇÃO/FichaPratica_Singleton/src/Ex_02/Logger.java
+++ PROGRAMAÇÃO/FichaPratica_Singleton/src/Ex_02/Logger.java
@@ -4,35 +4,34 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
 
 public class Logger {
 
-    private String nomeFicheiro;
+    private String caminhoFicheiro;
     private static Logger instance;    // Instância única da classe
 
     // Construtor privado para evitar instâncias diretas
-    private Logger(String nomeFicheiro) {
-        this.nomeFicheiro = nomeFicheiro;
+    private Logger(String caminhoFicheiro) {
+        this.caminhoFicheiro = caminhoFicheiro;
     }
 
     // Método estático para obter a instância única
-    public static synchronized Logger getInstance(String nomeFicheiro){
+    public static Logger getInstance(String caminhoFicheiro){
         if (instance == null){
-            instance = new Logger(nomeFicheiro);
+            instance = new Logger(caminhoFicheiro);
         }
         return instance;
     }
 
     // Método para gravar uma linha no arquivo de log
     public void log(String message) throws IOException {
-        File ficheiroLog = new File (this.nomeFicheiro);
+        File ficheiroLog = new File (this.caminhoFicheiro);
+        FileWriter fw = new FileWriter(ficheiroLog, true);
 
+        LocalDateTime localDateTime = LocalDateTime.now();
 
-        PrintWriter filewriter = new PrintWriter(new FileWriter(nomeFicheiro, true));
-        filewriter.println(message);
-        filewriter.close();
+        fw.append(localDateTime + ": " + message + "\n");
+        fw.close();
     }
-
-
-
 }
--- PROGRAMAÇÃO/FichaPratica_Singleton/src/Ex_03/UserSessionManager.java
+++ PROGRAMAÇÃO/FichaPratica_Singleton/src/Ex_03/UserSessionManager.java
@@ -2,46 +2,30 @@ package Ex_03;
 
 import Ex_02.Logger;
 
+import java.time.LocalDateTime;
 import java.util.Random;
 
 public class UserSessionManager {
-
-    private static UserSessionManager instance; // Instância única da classe
     private int sessionToken;
     private String lastAccess;
+
+    private static UserSessionManager instance; // Instância única da classe
 
     // Construtor privado para evitar instâncias diretas
     private UserSessionManager() { //Não recebe parametro por ser random
 
         Random random = new Random(); // Gera um token de sessão aleatório
-        gerarToken();
-        obterHoraAcesso();
+        this.sessionToken = random.nextInt();
+        this.lastAccess= String.valueOf(LocalDateTime.now());
     }
 
     // Método estático para obter a instância única
 
-    public static synchronized UserSessionManager getInstance() {
+    public static UserSessionManager getInstance() {
         if (instance == null) {
             instance = new UserSessionManager();
         }
         return instance;
-    }
-
-
-        public void gerarToken(){
-            Random random = new Random();
-            sessionToken = random.nextInt(10000);
-    }
-
-    public void obterHoraAcesso(){
-        // Atualiza o horário do último acesso
-        lastAccess = String.valueOf(System.currentTimeMillis());
-    }
-
-    public void atualizarUltimoAcesso(){
-        // Atualiza o horário do último acesso quando a sessão é atualizada
-        obterHoraAcesso();
-
     }
 
     public int getSessionToken() {
@@ -50,5 +34,9 @@ public class UserSessionManager {
 
     public String getLastAccess() {
         return lastAccess;
+    }
+
+    public void setLastAccess() {
+        this.lastAccess = String.valueOf(LocalDateTime.now());
     }
 }
