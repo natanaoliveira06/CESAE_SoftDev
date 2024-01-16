1ba6137e7ca5ea6821ba43ff1aca516854b42036
natanaoliveira06 <143542484+natanaoliveira06@users.noreply.github.com>
2024-01-16 18:25:06 +0000

    16/01/2024

---
+++ PROGRAMAÇÃO/ProjetoKotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    println("Program arguments: ${args.joinToString()}")
+
+    //var é VARIÁVEL MUTAVEL
+    var nome = "Natana Oliveira" //não é preciso ponto e virgula
+    nome= "Joana"
+    println(nome)
+
+    //var é VARIÁVEL IMUTAVEL
+}
