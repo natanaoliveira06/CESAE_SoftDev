44e8c165690cc8e64afb480001223ddac7598c02
natanaoliveira06 <143542484+natanaoliveira06@users.noreply.github.com>
2024-02-04 21:20:46 +0000

    04/02/2024

--- PHP_WEB.SERVERSIDE/Web_ServerSide/resources/views/main/fallback.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout.femaster')
-@section('content')
-
-
-    <h3>Vocês está no perdido. Por favor, <a href="{{route("bemvindos")}}">volte pra casa!</a></h3>
-
-
-@endsection
--- PHP_WEB.SERVERSIDE/Web_ServerSide/resources/views/users/add_user.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout.femaster')
-@section('content')
-
-
-    <h3>ADD USER:</h3>
-
-
-@endsection
